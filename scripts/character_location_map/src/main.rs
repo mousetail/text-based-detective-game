34cb44b2f26ac2ed8ec5c5ada2e83e96fd4c45c9
Maurits van Riezen (mousetail) <mousetail@mousetail.nl>
2024-04-28 07:46:59 +0200

    clean up styles

--- scripts/character_location_map/src/main.rs
+++ scripts/character_location_map/src/main.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashMap, fs::OpenOptions};
 
-use svg::{Color, SvgElement};
+use svg::{Color, LineStyle, SvgElement, TextStyle};
 
 use crate::svg::Vector;
 
@@ -15,10 +15,9 @@ const MIN_COLUMN_WIDTH: usize = 4;
 
 const EVENT_LINE_COLOR: svg::Color = Color("#222");
 const LOCATION_SEPERATOR_LINE_COLOR: Color = Color("#888");
-const LOCATION_TITLE_TEXT_COLOR: Color=Color("Black");
+const LOCATION_TITLE_TEXT_COLOR: Color = Color("Black");
 const EVENT_TEXT_COLOR: Color = Color("Black");
 const TIME_TEXT_COLOR: Color = Color("Black");
-
 
 const CHARACTER_COLORS: &'static [Color] = &[
     Color("Red"),
@@ -27,45 +26,70 @@ const CHARACTER_COLORS: &'static [Color] = &[
     Color("pink"),
     Color("Purple"),
     Color("Yellow"),
-    Color("Brown")
+    Color("Brown"),
 ];
 
-#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
-struct Character<'a>{name: &'a str}
+const LOCATION_HEADER_TEXT_STYLE: TextStyle = TextStyle {
+    align: svg::TextAlign::Center,
+    size: 12,
+    vertical_align: svg::TextVerticalAlign::Baseline,
+};
+
+const EVENT_NAME_TEXT_STYLE: TextStyle = TextStyle {
+    align: svg::TextAlign::Left,
+    size: 12,
+    vertical_align: svg::TextVerticalAlign::Baseline,
+};
+
+const LOCATION_SEPERATOR_LINE_STYLE: LineStyle = LineStyle {
+    dash_array: &[8, 8],
+    width: 2,
+};
+
+const EVENT_LINE_STYLE: LineStyle = LineStyle {
+    dash_array: &[],
+    width: 2,
+};
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
-struct Location<'a>{name: &'a str}
+struct Character<'a> {
+    name: &'a str,
+}
 
-#[derive(Clone, Debug, Eq, PartialEq)]
-struct Action<'a>{
-    characters: Vec<Character<'a>>,
-    name: &'a str
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
+struct Location<'a> {
+    name: &'a str,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
-struct Movement<'a>{
+struct Action<'a> {
     characters: Vec<Character<'a>>,
-    to: Location<'a>
+    name: &'a str,
 }
 
+#[derive(Clone, Debug, Eq, PartialEq)]
+struct Movement<'a> {
+    characters: Vec<Character<'a>>,
+    to: Location<'a>,
+}
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 struct Time<'a> {
-    time: &'a str
+    time: &'a str,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 enum Event<'a> {
     Action(Action<'a>),
     Movement(Movement<'a>),
-    time(Time<'a>)
+    time(Time<'a>),
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 struct Scene<'a> {
     locations: Vec<Location<'a>>,
     characters: Vec<Character<'a>>,
-    events: Vec<Event<'a>>
+    events: Vec<Event<'a>>,
 }
 
 type Story<'a> = Vec<Scene<'a>>;
@@ -81,26 +105,35 @@ fn generate_svg(story: Story) {
     svg::Svg {
         width: LEFT_BAR_WIDTH + MIDDLE_BAR_WIDTH + RIGHT_BAR_WIDTH,
         height: y,
-        elements
-    }.write("out.svg".to_string()).unwrap();
+        elements,
+    }
+    .write("out.svg".to_string())
+    .unwrap();
 }
 
 fn generate_svg_for_scene(scene: Scene, y: &mut usize) -> Vec<svg::SvgElement> {
     // first, find the people in each location
-    let mut people_per_location : Vec::<std::collections::HashMap::<Location, Vec<Character>>> = (0..scene.events.len()).map(|_|HashMap::new()).collect();
+    let mut people_per_location: Vec<std::collections::HashMap<Location, Vec<Character>>> =
+        (0..scene.events.len()).map(|_| HashMap::new()).collect();
 
-    let mut previous_people_per_location: std::collections::HashMap::<Location, Vec<Character>> = HashMap::new();
+    let mut previous_people_per_location: std::collections::HashMap<Location, Vec<Character>> =
+        HashMap::new();
     for (people, event) in people_per_location.iter_mut().zip(scene.events.iter()) {
         match event {
-            Event::Movement(Movement{characters, to}) => {
+            Event::Movement(Movement { characters, to }) => {
                 for character in characters {
-                    previous_people_per_location.values_mut().for_each(|value| value.retain(|c2|c2!=character));
+                    previous_people_per_location
+                        .values_mut()
+                        .for_each(|value| value.retain(|c2| c2 != character));
                 }
 
                 println!("Inserting {characters:?} into {to:?}");
-                previous_people_per_location.entry(to.clone()).or_insert(vec![]).extend(characters.iter().cloned());
-            },
-            _ => ()
+                previous_people_per_location
+                    .entry(to.clone())
+                    .or_insert(vec![])
+                    .extend(characters.iter().cloned());
+            }
+            _ => (),
         };
 
         *people = previous_people_per_location.clone();
@@ -108,14 +141,16 @@ fn generate_svg_for_scene(scene: Scene, y: &mut usize) -> Vec<svg::SvgElement> {
 
     println!("{people_per_location:#?}");
 
-
     let mut location_widths = std::collections::HashMap::<Location, usize>::new();
     for location in &scene.locations {
         location_widths.insert(
             location.clone(),
-             people_per_location.iter().map(
-                |k|k.get(&location).map_or(0, |t|t.len())
-            ).max().unwrap_or(0).max(MIN_COLUMN_WIDTH)
+            people_per_location
+                .iter()
+                .map(|k| k.get(&location).map_or(0, |t| t.len()))
+                .max()
+                .unwrap_or(0)
+                .max(MIN_COLUMN_WIDTH),
         );
     }
 
@@ -130,170 +165,187 @@ fn generate_svg_for_scene(scene: Scene, y: &mut usize) -> Vec<svg::SvgElement> {
             if let Some(people) = people_per_location.get(location) {
                 for person in people {
                     out.insert(person.clone(), x);
-                    x+=1;
+                    x += 1;
                 }
             }
             x = location_widths.get(location).unwrap_or(&0) + location_start_x;
             location_start_x = x;
-        };
+        }
         character_positions_by_time.push(out);
     }
 
     println!("{character_positions_by_time:#?}");
 
-
     let mut shapes: Vec<SvgElement> = vec![];
 
-    let mut x=0;
+    let mut x = 0;
 
-    *y+=VERTICAL_SPACING;
+    *y += VERTICAL_SPACING;
     for location in scene.locations {
-        shapes.push(
-            SvgElement::Text { color: LOCATION_TITLE_TEXT_COLOR, position: svg::Vector{x: LEFT_BAR_WIDTH + x * HORIZONTAL_SPACING, y: *y}, content: location.name.to_string() }
-        );
-        x+=location_widths.get(&location).unwrap_or(&0);
+        shapes.push(SvgElement::Text {
+            color: LOCATION_TITLE_TEXT_COLOR,
+            position: svg::Vector {
+                x: LEFT_BAR_WIDTH + x * HORIZONTAL_SPACING + (location_widths.get(&location).unwrap_or(&1) - 1) * HORIZONTAL_SPACING / 2,
+                y: *y,
+            },
+            content: location.name.to_string(),
+            style: LOCATION_HEADER_TEXT_STYLE,
+        });
+        x += location_widths.get(&location).unwrap_or(&0);
 
-        shapes.push(
-            SvgElement::Line { color: LOCATION_SEPERATOR_LINE_COLOR, 
-                dash_array: vec![8, 8],
-                points: vec![
-                svg::Vector{
+        shapes.push(SvgElement::Line {
+            color: LOCATION_SEPERATOR_LINE_COLOR,
+            style: LOCATION_SEPERATOR_LINE_STYLE,
+            points: vec![
+                svg::Vector {
                     x: x * HORIZONTAL_SPACING + LEFT_BAR_WIDTH - HORIZONTAL_SPACING / 2,
-                    y: *y
+                    y: *y + VERTICAL_SPACING / 2,
                 },
-                svg::Vector{
+                svg::Vector {
                     x: x * HORIZONTAL_SPACING + LEFT_BAR_WIDTH - HORIZONTAL_SPACING / 2,
-                    y: *y + VERTICAL_SPACING * character_positions_by_time.len()
+                    y: *y + VERTICAL_SPACING * character_positions_by_time.len(),
                 },
-            ] }
-        )
+            ],
+        })
     }
-    *y+=VERTICAL_SPACING;
+    *y += VERTICAL_SPACING;
 
-    for (person, color) in (&scene.characters).iter().zip(
-        CHARACTER_COLORS
-    ) {
+    for (person, color) in (&scene.characters).iter().zip(CHARACTER_COLORS) {
         let mut curves = vec![];
         let mut last_curve = vec![];
 
-        for (index, time) in character_positions_by_time.iter().chain(
-            std::iter::once(&HashMap::new())
-        ).enumerate() {
+        for (index, time) in character_positions_by_time
+            .iter()
+            .chain(std::iter::once(&HashMap::new()))
+            .enumerate()
+        {
             if let Some(x) = time.get(person) {
-                let new_point = svg::Vector{x: x * HORIZONTAL_SPACING + LEFT_BAR_WIDTH, y: index * VERTICAL_SPACING + *y};
+                let new_point = svg::Vector {
+                    x: x * HORIZONTAL_SPACING + LEFT_BAR_WIDTH,
+                    y: index * VERTICAL_SPACING + *y,
+                };
 
                 if (last_curve.len() == 0) {
-                    shapes.push(SvgElement::Circle { color: *color, position: new_point });
+                    shapes.push(SvgElement::Circle {
+                        color: *color,
+                        position: new_point,
+                    });
                 }
                 last_curve.push(new_point);
             } else if last_curve.len() > 0 {
                 let last_curve = std::mem::replace(&mut last_curve, vec![]);
                 let last_point = last_curve.last().unwrap();
-                shapes.push(SvgElement::Circle { color: *color, position: *last_point });
+                shapes.push(SvgElement::Circle {
+                    color: *color,
+                    position: *last_point,
+                });
                 curves.push(last_curve);
             }
         }
 
         for curve in curves {
-            shapes.push(
-                SvgElement::Curve { color: *color, points: curve }
-            )
+            shapes.push(SvgElement::Curve {
+                color: *color,
+                points: curve,
+            })
         }
     }
 
     for (index, event) in (&scene.events).iter().enumerate() {
         match event {
             Event::Movement(_) => (),
-            Event::Action(Action {
-                characters,
-                name
-            }) => {
-                shapes.push(SvgElement::Text { color: EVENT_TEXT_COLOR, position: Vector {
-                    x: LEFT_BAR_WIDTH + MIDDLE_BAR_WIDTH + HORIZONTAL_SPACING / 2,
-                    y: *y + index * VERTICAL_SPACING,
-                }, content: name.to_string() });
+            Event::Action(Action { characters, name }) => {
+                shapes.push(SvgElement::Text {
+                    color: EVENT_TEXT_COLOR,
+                    position: Vector {
+                        x: LEFT_BAR_WIDTH + MIDDLE_BAR_WIDTH + HORIZONTAL_SPACING / 2,
+                        y: *y + index * VERTICAL_SPACING,
+                    },
+                    content: name.to_string(),
+                    style: EVENT_NAME_TEXT_STYLE,
+                });
 
                 let mut min_x = usize::MAX;
                 for character in characters {
                     let character_x = *character_positions_by_time[index].get(character).unwrap();
-                    shapes.push(
-                        SvgElement::Circle { color: EVENT_LINE_COLOR, position: Vector {
+                    shapes.push(SvgElement::Circle {
+                        color: EVENT_LINE_COLOR,
+                        position: Vector {
                             x: character_x * HORIZONTAL_SPACING + LEFT_BAR_WIDTH,
-                            y: *y + index * VERTICAL_SPACING
-                        } }
-                    );
+                            y: *y + index * VERTICAL_SPACING,
+                        },
+                    });
                     min_x = min_x.min(character_x);
                 }
 
-                shapes.push(
-                    SvgElement::Line {
-                        color: EVENT_LINE_COLOR,
-                        dash_array: vec![],
-                        points: vec![
-                            Vector {
-                                x: LEFT_BAR_WIDTH + min_x * HORIZONTAL_SPACING,
-                                y: *y + index * VERTICAL_SPACING
-                            }, 
-                            Vector {
-                                x: LEFT_BAR_WIDTH + MIDDLE_BAR_WIDTH,
-                                y: *y + index * VERTICAL_SPACING
-                            }, 
-                        ]
-                    }
-                );
-            },
+                shapes.push(SvgElement::Line {
+                    color: EVENT_LINE_COLOR,
+                    style: EVENT_LINE_STYLE,
+                    points: vec![
+                        Vector {
+                            x: LEFT_BAR_WIDTH + min_x * HORIZONTAL_SPACING,
+                            y: *y + index * VERTICAL_SPACING,
+                        },
+                        Vector {
+                            x: LEFT_BAR_WIDTH + MIDDLE_BAR_WIDTH,
+                            y: *y + index * VERTICAL_SPACING,
+                        },
+                    ],
+                });
+            }
             Event::time(_) => todo!(),
         }
     }
 
-    *y+=scene.events.len() * VERTICAL_SPACING;
+    *y += scene.events.len() * VERTICAL_SPACING;
 
     return shapes;
 }
 
 fn main() {
-    let rufus_red = Character{name:"Rufus Red"};   
-    let dianna_robinson = Character{name:"Dianna Robinson"};
-    let judy_woolridge = Character{name:"Judy Woolridge"};
-    let duncan_moss = Character{name: "Duncan Moss"};
-    let rebecca_red = Character{name: "Rebecca Red"};
+    let rufus_red = Character { name: "Rufus Red" };
+    let dianna_robinson = Character {
+        name: "Dianna Robinson",
+    };
+    let judy_woolridge = Character {
+        name: "Judy Woolridge",
+    };
+    let duncan_moss = Character {
+        name: "Duncan Moss",
+    };
+    let rebecca_red = Character {
+        name: "Rebecca Red",
+    };
 
-    let dining_room = Location{name: "Dining Room"};
-    let garage = Location{name: "Garage"};
+    let dining_room = Location {
+        name: "Dining Room",
+    };
+    let garage = Location { name: "Garage" };
 
-    generate_svg(vec![
-        Scene{
-            locations: vec![
-                dining_room,
-                garage
-            ],
-            characters: vec![
-                rufus_red,
-                dianna_robinson
-            ],
-            events: vec![
-
-            Event::Movement(Movement{
+    generate_svg(vec![Scene {
+        locations: vec![dining_room, garage],
+        characters: vec![rufus_red, dianna_robinson],
+        events: vec![
+            Event::Movement(Movement {
                 characters: vec![rufus_red],
-                to: dining_room.clone()
+                to: dining_room.clone(),
             }),
-            Event::Action(Action{
+            Event::Action(Action {
                 characters: vec![rufus_red],
-                name: "Rufus finds the note"
+                name: "Rufus finds the note",
             }),
-            Event::Movement(Movement{
+            Event::Movement(Movement {
                 characters: vec![rufus_red, dianna_robinson],
-                to: garage.clone()
+                to: garage.clone(),
             }),
-            Event::Action(Action{
+            Event::Action(Action {
                 characters: vec![rufus_red, dianna_robinson],
-                name: "Dianna and Rufus Argue"
+                name: "Dianna and Rufus Argue",
             }),
-            Event::Movement(Movement{
+            Event::Movement(Movement {
                 characters: vec![rufus_red, dianna_robinson],
-                to: dining_room
-            })
-            ]
-        },
-    ]);
+                to: dining_room,
+            }),
+        ],
+    }]);
 }
--- scripts/character_location_map/src/svg.rs
+++ scripts/character_location_map/src/svg.rs
@@ -1,113 +1,210 @@
+use std::fmt::Display;
 use std::fs::OpenOptions;
 use std::io::Write;
-
 
 #[derive(Clone, Debug, Eq, PartialEq, Copy)]
 pub struct Color(pub &'static str);
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
-pub struct Vector{
+pub struct Vector {
     pub x: usize,
-    pub y: usize
+    pub y: usize,
 }
 
 pub enum SvgElement {
-    Circle{color: Color, position: Vector},
-    Curve{color: Color, points: Vec<Vector>},
-    Line{color: Color, points: Vec<Vector>, dash_array: Vec<usize>},
-    Text{color: Color, position: Vector, content: String}
+    Circle {
+        color: Color,
+        position: Vector,
+    },
+    Curve {
+        color: Color,
+        points: Vec<Vector>,
+    },
+    Line {
+        color: Color,
+        points: Vec<Vector>,
+        style: LineStyle,
+    },
+    Text {
+        color: Color,
+        position: Vector,
+        content: String,
+        style: TextStyle,
+    },
 }
 
 pub struct Svg {
     pub width: usize,
     pub height: usize,
-    pub elements: Vec<SvgElement>
+    pub elements: Vec<SvgElement>,
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub struct LineStyle {
+    pub dash_array: &'static [usize],
+    pub width: usize,
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub enum TextAlign {
+    Left,
+    Center,
+    Right,
+}
+
+impl Display for TextAlign {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TextAlign::Left => write!(f, "start"),
+            TextAlign::Center => write!(f, "middle"),
+            TextAlign::Right => write!(f, "end"),
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub enum TextVerticalAlign {
+    Top,
+    Baseline,
+}
+
+impl Display for TextVerticalAlign {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TextVerticalAlign::Top => write!(f, "hanging"),
+            TextVerticalAlign::Baseline => write!(f, "alphabetic"),
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub struct TextStyle {
+    pub size: usize,
+    pub align: TextAlign,
+    pub vertical_align: TextVerticalAlign,
 }
 
 impl Svg {
     pub fn write(&self, filename: String) -> std::io::Result<()> {
         println!("About to create file");
-        let mut file = OpenOptions::new().write(true).create(true).truncate(true).open("out.svg")?;
+        let mut file = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open("out.svg")?;
         println!("File created");
-        write!(file, r#"
+        write!(
+            file,
+            r#"
         <svg version="1.1"
             width="{width}" height="{height}"
             viewBox="0 0 {width} {height}"
             xmlns="http://www.w3.org/2000/svg">
-        "#, width=self.width, height=self.height)?;
+        "#,
+            width = self.width,
+            height = self.height
+        )?;
 
         for element in &self.elements {
             match element {
-                SvgElement::Circle { color, position: Vector{ x, y} } => write!(file, r#"
-                    <circle cx="{x}" cy="{y}" fill="{color}" r="5"/>
-                "#, color=xml::escape::escape_str_attribute(&color.0))?,
-                SvgElement::Curve { color, points } => {
-                    write!(file, r#"
-                        <path stroke="{color}" stroke-width="2" fill="transparent"  d="
-                    "#, color=xml::escape::escape_str_attribute(&color.0))?;
-
-                    if let Some((first, rest)) = points.split_first(){
-                        write!(
-                            file, "M {} {} ", first.x, first.y
-                        )?;
-
-                        let mut previous = first;
-
-                        for point in rest {
-                            write!(
-                                file, "C {} {} {} {} {} {} ",
-                                previous.x, (previous.y * 2 + point.y) / 3,
-                                point.x, (previous.y + point.y * 2) / 3,
-                                point.x, point.y
-                            )?; 
-
-                            previous = point;
-                        }
-                    }
-
-                    write!(
-                        file, r#""/>"#
-                    )?;
-                },
-                SvgElement::Line { color, points, dash_array } => {
-                    write!(file, r#"
-                        <path stroke="{color}" stroke-width="2" fill="transparent" stroke-dasharray="{dash_array}" d="
-                    "#, color=xml::escape::escape_str_attribute(&color.0), dash_array=dash_array.iter().map(|k|format!("{k} ")).collect::<String>())?;
-
-                    if let Some((first, rest)) = points.split_first(){
-                        write!(
-                            file, "M {} {} ", first.x, first.y
-                        )?;
-
-                        let mut previous = first;
-
-                        for point in rest {
-                            write!(
-                                file, "L {} {} ",
-                                point.x, point.y
-                            )?; 
-
-                            previous = point;
-                        }
-                    }
-
-                    write!(
-                        file, r#""/>"#
-                    )?;
-                },
-                SvgElement::Text { color, position: Vector{ x, y}, content } => write!(file,
+                SvgElement::Circle {
+                    color,
+                    position: Vector { x, y },
+                } => write!(
+                    file,
                     r#"
-                        <text x="{x}" y="{y}" fill="{color}">{content}</text>
+                    <circle cx="{x}" cy="{y}" fill="{color}" r="5"/>
+                "#,
+                    color = xml::escape::escape_str_attribute(&color.0)
+                )?,
+                SvgElement::Curve { color, points } => {
+                    write!(
+                        file,
+                        r#"
+                        <path stroke="{color}" stroke-width="2" fill="transparent"  d="
                     "#,
-                    color=xml::escape::escape_str_attribute(&color.0),
-                    content=xml::escape::escape_str_pcdata(&content)
+                        color = xml::escape::escape_str_attribute(&color.0)
+                    )?;
+
+                    if let Some((first, rest)) = points.split_first() {
+                        write!(file, "M {} {} ", first.x, first.y)?;
+
+                        let mut previous = first;
+
+                        for point in rest {
+                            write!(
+                                file,
+                                "C {} {} {} {} {} {} ",
+                                previous.x,
+                                (previous.y * 2 + point.y) / 3,
+                                point.x,
+                                (previous.y + point.y * 2) / 3,
+                                point.x,
+                                point.y
+                            )?;
+
+                            previous = point;
+                        }
+                    }
+
+                    write!(file, r#""/>"#)?;
+                }
+                SvgElement::Line {
+                    color,
+                    points,
+                    style,
+                } => {
+                    write!(
+                        file,
+                        r#"
+                        <path stroke="{color}" stroke-width="2" fill="transparent" stroke-dasharray="{dash_array}" d="
+                    "#,
+                        color = xml::escape::escape_str_attribute(&color.0),
+                        dash_array = style
+                            .dash_array
+                            .iter()
+                            .map(|k| format!("{k} "))
+                            .collect::<String>()
+                    )?;
+
+                    if let Some((first, rest)) = points.split_first() {
+                        write!(file, "M {} {} ", first.x, first.y)?;
+
+                        let mut previous = first;
+
+                        for point in rest {
+                            write!(file, "L {} {} ", point.x, point.y)?;
+
+                            previous = point;
+                        }
+                    }
+
+                    write!(file, r#""/>"#)?;
+                }
+                SvgElement::Text {
+                    color,
+                    position: Vector { x, y },
+                    content,
+                    style,
+                } => write!(
+                    file,
+                    r#"
+                        <text x="{x}"
+                              y="{y}"
+                              fill="{color}"
+                              dominant-baseline="{dominant_baseline}"
+                              text-anchor="{text_anchor}"
+                        >{content}</text>
+                    "#,
+                    color = xml::escape::escape_str_attribute(&color.0),
+                    content = xml::escape::escape_str_pcdata(&content),
+                    dominant_baseline = style.vertical_align,
+                    text_anchor = style.align
                 )?,
             }
         }
 
-        write!(
-            file, "</svg>"
-        )?;
+        write!(file, "</svg>")?;
 
         return Ok(());
     }
